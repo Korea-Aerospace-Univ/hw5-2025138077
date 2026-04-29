d773371344e61437ffb734534c7488bdd2037d60
2025138077 <ddolieee@kau.kr>
2026-04-29 16:17:02 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -3,21 +3,21 @@
 int main() {
   int N;
   int A = 900, B = 750, C = 200;
-  int found = 0;
+  bool found = false;
   scanf("%d", &N);
   
   for(int a = 1; a * A <= N; a++) {
-    for(int b = 2; a * A + b * B <= N; b += 2) {
-        for(int c = 1; a * A + b * B + c * C <= N; c++) {
+    for(int b = 2; b * B <= N; b += 2) {
+        for(int c = 1; c * C <= N; c++) {
             int total = a * A + b * B + c * C;
-            if(total == N && (a > 0 && b > 0 && c > 0) && (c < a || c < b)) {
-                found = 1;
+            if(total == N && (c < a || c < b)) {
+                found = true;
                 printf("%d %d %d\n", a, b, c);
             }
         }
     }
   }
-  if(found == 0)
+  if(!found)
     printf("none");
   
   return 0;
